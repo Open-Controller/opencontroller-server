ae5124408bcb345dcdbf15d1441a6f90d5d94faf
Peter PJT <pjtsignups@gmail.com>
2020-08-18 10:56:05 -0700

    feat: send content type json

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ export const createServer = (house:House,port:number=3612)=>{
   const app = new Koa();
 
   app.use(async ctx => {
-    ctx.is('application/json');
-    ctx.body = JSON.stringify(house);
+    ctx.body = house;
   });
 
   app.listen(port);
